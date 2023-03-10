137531773dc2f1dfdc4bc3925ab9c8828aa23724
x29Naybla <29Naybla@gmail.com>
2023-03-10 16:34:19 -0300

    Soy milk bottle stacks to 16; Soybeans compostable;

--- src/main/java/com/x29naybla/simplesoy/SimpleSoy.java
+++ src/main/java/com/x29naybla/simplesoy/SimpleSoy.java
@@ -3,16 +3,7 @@ package com.x29naybla.simplesoy;
 import com.mojang.logging.LogUtils;
 import com.x29naybla.simplesoy.block.ModBlocks;
 import com.x29naybla.simplesoy.item.ModItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,11 +11,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -44,9 +31,11 @@ public class SimpleSoy
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
+        ModItems.registerCompostables();
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
@@ -54,7 +43,6 @@ public class SimpleSoy
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-
         }
     }
 }
--- src/main/java/com/x29naybla/simplesoy/item/ModItems.java
+++ src/main/java/com/x29naybla/simplesoy/item/ModItems.java
@@ -4,6 +4,7 @@ import com.x29naybla.simplesoy.SimpleSoy;
 import com.x29naybla.simplesoy.block.ModBlocks;
 import com.x29naybla.simplesoy.item.custom.SoyMilkItem;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,11 +18,13 @@ public class ModItems {
             () -> new ItemNameBlockItem(ModBlocks.SOY_CROP.get(),
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     public static final RegistryObject<Item> SOY_MILK = ITEMS.register("soy_milk",
-            () -> new SoyMilkItem(new SoyMilkItem.Properties().tab(CreativeModeTab.TAB_MISC).food(ModFoods.SOY_MILK).stacksTo(1).craftRemainder(Items.GLASS_BOTTLE)));
+            () -> new SoyMilkItem(new SoyMilkItem.Properties().tab(CreativeModeTab.TAB_MISC).food(ModFoods.SOY_MILK).stacksTo(16).craftRemainder(Items.GLASS_BOTTLE)));
     public static final RegistryObject<Item> TOFU = ITEMS.register("tofu",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.TOFU)));
 
-
+    public static void registerCompostables() {
+        ComposterBlock.COMPOSTABLES.put(ModItems.SOYBEANS.get(), 0.3F);
+    }
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
