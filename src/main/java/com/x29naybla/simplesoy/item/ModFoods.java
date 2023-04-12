57fb3c61c3e28388ad526d5baa9d2efa902a3414
x29Naybla <29Naybla@gmail.com>
2023-04-12 10:07:44 -0300

    0.0.5;

--- src/main/java/com/x29naybla/simplesoy/item/ModFoods.java
+++ src/main/java/com/x29naybla/simplesoy/item/ModFoods.java
@@ -5,4 +5,5 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties SOY_MILK = (new FoodProperties.Builder()).nutrition(0).saturationMod(0F).alwaysEat().build();
     public static final FoodProperties TOFU = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.6F).build();
+    public static final FoodProperties MISO_SOUP = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.6F).build();
 }
--- src/main/java/com/x29naybla/simplesoy/item/ModItems.java
+++ src/main/java/com/x29naybla/simplesoy/item/ModItems.java
@@ -16,10 +16,13 @@ public class ModItems {
     public static final RegistryObject<Item> SOYBEANS = ITEMS.register("soybeans",
             () -> new ItemNameBlockItem(ModBlocks.SOY_CROP.get(),
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
     public static final RegistryObject<Item> SOY_MILK = ITEMS.register("soy_milk",
             () -> new SoyMilkItem(new SoyMilkItem.Properties().tab(CreativeModeTab.TAB_MISC).food(ModFoods.SOY_MILK).stacksTo(16).craftRemainder(Items.GLASS_BOTTLE)));
     public static final RegistryObject<Item> TOFU = ITEMS.register("tofu",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.TOFU)));
+    public static final RegistryObject<Item> MISO_SOUP = ITEMS.register("miso_soup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.MISO_SOUP).stacksTo(1).craftRemainder(Items.BOWL)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
